dffc6be191db605d3007ca407e7b9bda284e17c6
Ritu Anuragi <81502161+rituanuragi@users.noreply.github.com>
2024-06-10 13:16:46 +0530

    Update dsaregister.js

--- src/components/dsaregister.js
+++ src/components/dsaregister.js
@@ -90,7 +90,7 @@ const DSARegistrationForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch("http://35.154.194.202:5000/register-dsa", {
+    fetch("http://13.202.31.219:5000/submitdsa", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
